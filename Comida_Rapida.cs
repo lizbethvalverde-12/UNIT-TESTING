9fec5cd978956ccbcfce77acc1c6794a42ad22ce
lizbethvalverde-12 <lizbeth.valverde@estudiant.fjaverianas.com>
2026-06-04 22:05:57 +0200

    Update Comida_Rapida.cs

--- Comida_Rapida.cs
+++ Comida_Rapida.cs
@@ -13,7 +13,6 @@ public class TelepizzaVirtual
         return codigo == "PROFE10" ? total - 10 : total;
     }
 
-    // 3. Suplemento extra (+1.50€)
     public double ConQuesoExtra(double precioBase)
     {
         return precioBase + 1.50;
@@ -29,9 +28,9 @@ public class TelepizzaVirtual
     }
 }
 
-//He decidido crear un sistema de gestión de pedidos de comida rápida. El código se ha diseñado con lo metodos del  Clean Code, utilizando nombres de funciones y variables descriptivas en español para que sea fácil de entender.
-//Para asegurar que el proyecto es totalmente robusto, se han programado 5 casos de prueba distintos utilizando el framework xUnit.
+//He decidido crear un sistema de gestión de pedidos de comida rápida. El código se ha diseñado con lo metodos del  Clean Code, utilizando nombres de funciones y variables descriptivas para que sea fácil de entender.
+//Para asegurar que el proyecto es funcional al 100%, se han programado 5 casos de prueba distintos utilizando el framework xUnit.
 
 /*Cada test incluye un mínimo de 3 aserciones (Assert).
 
-No solo se prueban los "caminos felices" (cuando todo va bien), sino también los casos límite y valores erróneos (por ejemplo, comprobar qué pasa si un cliente mete un código de descuento falso, o si intenta pedir comida a las 4 de la mañana cuando el restaurante está cerrado).*/
+ Comprobar qué pasa si un cliente mete un código de descuento falso, o si intenta pedir comida a las 4 de la mañana cuando el restaurante está cerrado).*/
